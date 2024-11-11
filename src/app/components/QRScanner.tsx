2bcdb22883c50284c0191fb38f1135cdf617a233
soomyinthtel <gretaoto44@gmail.com>
2024-11-11 17:02:31 +0630

    testing qr

--- src/app/components/QRScanner.tsx
+++ src/app/components/QRScanner.tsx
@@ -4,8 +4,11 @@ import { Html5Qrcode } from "html5-qrcode";
 const QrScanner: React.FC = () => {
   const qrCodeRef = useRef<HTMLDivElement | null>(null);
   const [html5QrCode, setHtml5QrCode] = useState<Html5Qrcode | null>(null);
+  const [facingMode, setFacingMode] = useState("environment");
   const [scanning, setScanning] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null); // To capture errors for UI feedback
+  const [error, setError] = useState<string | null>(null);
+  const [qrCodeResult, setQrCodeResult] = useState<string | null>(null); // Store the result of image scan
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
     const qrCodeInstance = new Html5Qrcode("qr-reader");
@@ -18,6 +21,12 @@ const QrScanner: React.FC = () => {
     };
   }, []);
 
+  //   const switchCamera = () => {
+  //     const newFacingMode = facingMode === "user" ? "environment" : "user";
+  //     setFacingMode(newFacingMode);
+  //     stopScanning(); // Stop scanning before switching the camera
+  //   };
+
   const startScanning = () => {
     if (!html5QrCode) return;
 
@@ -28,12 +37,12 @@ const QrScanner: React.FC = () => {
 
     html5QrCode
       .start(
-        { facingMode: "user" }, // Use user camera
+        { facingMode }, // Use user camera
         config,
         (decodedText: string) => {
           console.log(`QR Code scanned: ${decodedText}`);
-          stopScanning();
-          // Handle successful scan here (e.g., display result)
+          setQrCodeResult(decodedText); // Set result of the scan
+          stopScanning(); // Stop scanning after successful scan
         },
         (errorMessage: string) => {
           console.log(`Error scanning: ${errorMessage}`);
@@ -41,7 +50,7 @@ const QrScanner: React.FC = () => {
       )
       .then(() => {
         setScanning(true);
-        setError(null); // Reset error state on successful start
+        setError(null);
       })
       .catch((err) => {
         setError(`Error starting QR code scanner: ${err}`);
@@ -62,21 +71,72 @@ const QrScanner: React.FC = () => {
       });
   };
 
+  const handleImageScan = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+
+    const file = event.target.files[0];
+    if (!file) return;
+    if (!html5QrCode) return;
+    html5QrCode
+      .scanFile(file)
+      .then((decodedText) => {
+        console.log(`QR Code scanned from image: ${decodedText}`);
+        setQrCodeResult(decodedText); // Set result from image scan
+      })
+      .catch((err) => {
+        setError(`Error scanning QR code from image: ${err}`);
+      });
+  };
+
   return (
-    <div>
-      <div>
-        <h1 className="text-2xl font-semibold mb-4">Scan QR Code</h1>
-        <div id="qr-reader" ref={qrCodeRef} className="w-full h-full"></div>
+    <div className="flex flex-col justify-between h-screen py-4 px-2">
+      <div className="flex items-center justify-between px-2">
+        <div>🔙</div>
+        <p className="font-semibold">Scan QR Code</p>
+        <div>
+          <button onClick={() => fileInputRef.current?.click()}>Album</button>
+          <input
+            ref={fileInputRef}
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={handleImageScan}
+          />
+        </div>
+      </div>
+      <div className="relative">
+        <div
+          id="qr-reader"
+          ref={qrCodeRef}
+          className="w-full h-auto z-10"
+        ></div>
+        <p className="w-full absolute p-4 z-20 text-center transform -translate-x-1/2 -translate-y-1/2 bottom-0 left-1/2 text-white">
+          Align QR code within frame to scan
+        </p>
       </div>
       {error && <div style={{ color: "red", marginTop: "10px" }}>{error}</div>}
-      <div style={{ marginTop: "10px" }}>
-        <button onClick={startScanning} disabled={scanning}>
-          Start Scanning
+      {qrCodeResult && (
+        <div style={{ marginTop: "10px" }}>
+          <p>QR Code result: {qrCodeResult}</p>
+        </div>
+      )}
+      <div className="flex items-center justify-between px-2">
+        <button
+          onClick={startScanning}
+          disabled={scanning}
+          className="border p-2 rounded-sm"
+        >
+          Start Scanning (Camera)
         </button>
-        <button onClick={stopScanning} disabled={!scanning}>
+        <button
+          onClick={stopScanning}
+          disabled={!scanning}
+          className="border p-2 rounded-sm"
+        >
           Stop Scanning
         </button>
       </div>
+      {/* <button onClick={switchCamera}>Switch Camera</button> */}
     </div>
   );
 };
